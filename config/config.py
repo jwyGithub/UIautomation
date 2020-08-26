8518d1e4698ac39cf498dca82fcc2fc577287134
jiangweiye <jiangweiye@deppon.com>
2020-08-26 15:58:28 +0800

    test

--- config/config.py
+++ config/config.py
@@ -2,4 +2,4 @@
 
 
 def config():
-    return {"url": "http://itssit.deppontest.com:8080/its-opt-web/login/index.action", "account": "000000", "pwd": "dpuums."}
+    return {"url": "http:www.jwy.com", "account": "000000", "pwd": "00000000"}
